ce344a19b9f021914e4cb8bc16488988e985ba7c
Robpol86 <robpol86@gmail.com>
2016-01-07 21:54:36 -0800

    File discovery code and logic.
    discover module handles walking directories, reading file metadata,
    deciding which file should be converted, which has already been
    convered, which abandoned file to delete, and which empty directory to
    remove.

---
+++ flash_air_music/convert/discover.py
@@ -0,0 +1,106 @@
+"""Walk directories looking for source/target music files.
+
+Target mp3 files hold source metadata in their ID3 comment tags. Each mp3 file is like a little database of itself.
+"""
+
+import os
+
+from flash_air_music.convert.id3_flac_tags import read_stored_metadata
+
+VALID_SOURCE_EXTENSIONS = ('.flac', '.mp3')
+
+
+class Song(object):
+    """Holds information about one song. Handles source/destination file paths.
+
+    :ivar str source: Source file path (usually FLAC file).
+    :ivar str target: Target file path (mp3 file).
+    :ivar dict stored_metadata: Previously recorded metadata of source and target files stored in target file ID3 tag.
+    """
+
+    def __init__(self, source, source_dir, target_dir):
+        """Constructor."""
+        self.source = source
+
+        # Determine target path.
+        target_path_old_extension = os.path.join(target_dir, os.path.relpath(source, source_dir))
+        self.target = os.path.splitext(target_path_old_extension)[0] + '.mp3'
+
+        # Read previous metadata from target file.
+        self.previous_metadata = read_stored_metadata(self.target)
+        self.current_metadata = dict()
+        self.refresh_current_metadata()
+
+    @property
+    def needs_conversion(self):
+        """Skip file if nothing has changed."""
+        return self.previous_metadata != self.current_metadata
+
+    def refresh_current_metadata(self):
+        """Read current file metadata of source and target file."""
+        source_stat = os.stat(self.source)
+        self.current_metadata['source_mtime'] = int(source_stat.st_mtime)
+        self.current_metadata['source_size'] = int(source_stat.st_size)
+        try:
+            target_stat = os.stat(self.target)
+            self.current_metadata['target_mtime'] = int(target_stat.st_mtime)
+            self.current_metadata['target_size'] = int(target_stat.st_size)
+        except FileNotFoundError:
+            self.current_metadata['target_mtime'] = 0
+            self.current_metadata['target_size'] = 0
+
+
+def get_songs(source_dir, target_dir):
+    """Walk source and target directories looking for files to convert.
+
+    :param str source_dir: Source directory.
+    :param str target_dir: Target directory.
+
+    :return: Song instances that need conversion and list of all mp3 targets targets that need or don't need conversion.
+    """
+    source_dir = os.path.realpath(source_dir)
+    target_dir = os.path.realpath(target_dir)
+    valid_targets = list()
+    songs = list()
+
+    for path in (os.path.join(root, f) for root, _, files in os.walk(source_dir) for f in files):
+        if os.path.splitext(path)[1].lower() not in VALID_SOURCE_EXTENSIONS:
+            continue
+        song = Song(path, source_dir, target_dir)
+        valid_targets.append(song.target)
+        if song.needs_conversion:
+            songs.append(song)
+
+    return songs, valid_targets
+
+
+def files_dirs_to_delete(target_dir, valid_targets):
+    """Walk source and target directories looking for files to delete and empty directories to remove.
+
+    :param str target_dir: Target directory.
+    :param iter valid_targets: List of valid target files from get_songs().
+
+    :return: Abandoned files to delete and empty directories to remove.
+    :rtype: tuple
+    """
+    target_dir = os.path.realpath(target_dir)
+    delete_files = set()
+    remove_dirs = set()
+
+    # Discover abandoned target files.
+    for path in (os.path.join(root, f) for root, _, files in os.walk(target_dir) for f in files):
+        if path in valid_targets:
+            continue
+        if path.lower().endswith('.mp3'):
+            delete_files.add(path)
+
+    # Discover empty directories.
+    for root, files in ((r, {os.path.join(r, f) for f in fs}) for r, _, fs in os.walk(target_dir)):
+        if root == target_dir:
+            continue
+        if not files:
+            remove_dirs.add(root)
+        elif not files - delete_files:
+            remove_dirs.add(root)
+
+    return delete_files, remove_dirs
---
+++ tests/test_convert/test_discover.py
@@ -0,0 +1,129 @@
+"""Test functions in module."""
+
+import json
+import os
+import time
+
+import py
+import pytest
+from mutagen.id3 import COMM, ID3
+
+from flash_air_music.convert import discover, id3_flac_tags
+
+HERE = py.path.local(__file__).dirpath()
+
+
+@pytest.mark.parametrize('mode', ['no target', 'no prev metadata', 'st', 'ss', 'tt', 'ts', 'up to date'])
+def test_song(tmpdir, mode):
+    """Test Song class.
+
+    :param tmpdir: pytest fixture.
+    :param str mode: Scenario to test for.
+    """
+    source_file = tmpdir.join('source').ensure_dir().join('song.mp3')
+    target_file = tmpdir.join('target').ensure_dir().join('song.mp3')
+    HERE.join('1khz_sine.mp3').copy(source_file)
+
+    # Write metadata.
+    if mode != 'no target':
+        source_file.copy(target_file)
+        if mode != 'no prev metadata':
+            ID3(str(target_file)).save(padding=lambda _: 200)
+            metadata = dict(
+                source_mtime=int(source_file.stat().mtime) - (20 if mode == 'st' else 0),
+                source_size=int(source_file.stat().size) - (20 if mode == 'ss' else 0),
+                target_mtime=int(target_file.stat().mtime) - (20 if mode == 'tt' else 0),
+                target_size=int(target_file.stat().size) - (20 if mode == 'ts' else 0),
+            )
+            atime, mtime = time.time(), target_file.stat().mtime
+            id3 = ID3(str(target_file))
+            id3.add(COMM(desc=id3_flac_tags.COMMENT_DESCRIPTION, encoding=3, lang='eng', text=json.dumps(metadata)))
+            id3.save()
+            os.utime(str(target_file), (atime, mtime))
+
+    # Run.
+    song = discover.Song(str(source_file), source_file.dirname, target_file.dirname)
+
+    # Verify.
+    assert song.source == str(source_file)
+    assert song.target == str(target_file)
+    assert song.needs_conversion is (False if mode == 'up to date' else True)
+
+
+def test_get_songs(tmpdir):
+    """Test get_songs() function.
+
+    :param tmpdir: pytest fixture.
+    """
+    source_dir = tmpdir.join('source').ensure_dir()
+    target_dir = tmpdir.join('target').ensure_dir()
+
+    # Test empty.
+    songs, valid_targets = discover.get_songs(str(source_dir), str(target_dir))
+    assert not songs
+    assert not valid_targets
+
+    # Test ignore.
+    source_dir.join('ignore.txt').ensure()
+    songs, valid_targets = discover.get_songs(str(source_dir), str(target_dir))
+    assert not songs
+    assert not valid_targets
+
+    # Create mp3 that needs conversion and FLAC that doesn't.
+    HERE.join('1khz_sine.mp3').copy(source_dir.join('song1.mp3'))
+    HERE.join('1khz_sine.flac').copy(source_dir.join('song2.flac'))
+    HERE.join('1khz_sine.mp3').copy(target_dir.join('song2.mp3'))
+    ID3(str(target_dir.join('song2.mp3'))).save(padding=lambda _: 200)
+    metadata = dict(
+        source_mtime=int(source_dir.join('song2.flac').stat().mtime),
+        source_size=int(source_dir.join('song2.flac').stat().size),
+        target_mtime=int(target_dir.join('song2.mp3').stat().mtime),
+        target_size=int(target_dir.join('song2.mp3').stat().size),
+    )
+    atime, mtime = time.time(), target_dir.join('song2.mp3').stat().mtime
+    id3 = ID3(str(target_dir.join('song2.mp3')))
+    id3.add(COMM(desc=id3_flac_tags.COMMENT_DESCRIPTION, encoding=3, lang='eng', text=json.dumps(metadata)))
+    id3.save()
+    os.utime(str(target_dir.join('song2.mp3')), (atime, mtime))
+
+    # Test those files.
+    songs, valid_targets = discover.get_songs(str(source_dir), str(target_dir))
+    assert len(songs) == 1
+    assert len(valid_targets) == 2
+    assert songs[0].source == str(source_dir.join('song1.mp3'))
+    assert songs[0].target == str(target_dir.join('song1.mp3'))
+    assert sorted(valid_targets) == [str(target_dir.join('song1.mp3')), str(target_dir.join('song2.mp3'))]
+
+
+def test_files_dirs_to_delete(tmpdir):
+    """Test files_dirs_to_delete() function.
+
+    :param tmpdir: pytest fixture.
+    """
+    target_dir = tmpdir.join('target').ensure_dir()
+    valid_targets = list()
+
+    # Test empty.
+    delete_files, remove_dirs = discover.files_dirs_to_delete(str(target_dir), valid_targets)
+    assert not delete_files
+    assert not remove_dirs
+
+    # Test ignore.
+    target_dir.join('ignore.txt').ensure()
+    target_dir.join('not_empty').ensure_dir().join('ignore.txt').ensure()
+    delete_files, remove_dirs = discover.files_dirs_to_delete(str(target_dir), valid_targets)
+    assert not delete_files
+    assert not remove_dirs
+
+    # Setup filesystem.
+    expected_delete, expected_remove = set(), {str(target_dir.join('remove_this_dir').ensure_dir())}
+    expected_delete.add(str(target_dir.join('remove_this_dir', 'remove_me.mp3').ensure()))
+    expected_delete.add(str(target_dir.join('remove_me_too.mp3').ensure()))
+    expected_remove.add(str(target_dir.join('remove_this_dir_too').ensure_dir()))
+    valid_targets.append(str(target_dir.join('keep_me.mp3').ensure()))
+    valid_targets.append(str(target_dir.join('keep_this').ensure_dir().join('keep_me.mp3').ensure()))
+
+    # Test those files.
+    delete_files, remove_dirs = discover.files_dirs_to_delete(str(target_dir), valid_targets)
+    assert delete_files == expected_delete
+    assert remove_dirs == expected_remove
