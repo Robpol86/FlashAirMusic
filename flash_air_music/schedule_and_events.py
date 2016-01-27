a91ff9eaa963f74a80033f03651ffadcbb0a90ec
Robpol86 <robpol86@gmail.com>
2016-01-26 21:22:59 -0800

    Nooo.
    Finally I forgot to remove this file before committing.

--- flash_air_music/schedule_and_events.py
+++
@@ -1,6 +0,0 @@
-"""TODO.
-
-* Add --fork option for container testing. Or stackoverflow.com/questions/1908610/how-to-get-pid-of-background-process
-* Need to make periodic timers an option to properly test. Then re-add:
-     assert 'periodically_convert() saw shutdown signal.' in stdout
-"""
